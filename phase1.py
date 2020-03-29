85a25d80036dda4e1b70499074fad5a23e99d7c8
jieyuan-bi <55514311+jieyuan-bi@users.noreply.github.com>
2020-03-28 18:38:54 -0600

    Update phase1.py

--- phase1.py
+++ phase1.py
@@ -1,1 +1,17 @@
-print(1)
+import os
+
+def main():
+    file_names =['reviews.txt','scores.txt','pterms.txt','rterms.txt']
+    idx_names =['rw.idx','sc.idx','pt.idx','rt.idx']
+    structures =['hash','btree','btree','btree']
+    for i in range(len(file_names)):
+        structure = structures[i]
+        if structure == 'btree':
+            add = "-c duplicates=1"
+        elif structure == 'hash':
+            add = ""
+        os.system("sort -u %s -o %s"%(file_names[i],file_names[i]))
+        os.system("perl break.pl < %s | db_load -T -t %s %s %s"\
+                  %(file_names[i],structure,add,idx_names[i]))
+
+main()
